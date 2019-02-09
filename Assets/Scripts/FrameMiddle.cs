001c14bf85847e6153da1c0d340f0a41764830ab
JDAV96 <arevalojose2015@outlook.com>
2019-02-09 15:53:11 -0800

    Colour changing frame

---
+++ Assets/Scripts/FrameMiddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameMiddle : MonoBehaviour
+{
+    private SpriteRenderer spriteR;
+    Color currentColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeColorPeriodically();
+    }
+
+    void ChangeColorPeriodically()
+    {
+        spriteR = gameObject.GetComponent<SpriteRenderer>();
+
+        var lerpedColor = Color.Lerp(Color.white, Color.green, Mathf.PingPong(Time.time, 1));
+
+        spriteR.color = lerpedColor;
+    }
+
+}
