4d99d106d8977fe2d36dba76952eba33a35a494d
JDAV96 <arevalojose2015@outlook.com>
2019-02-09 16:19:01 -0800

    Setting up Spawners and alpha Activators

---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningManager : MonoBehaviour
+{
+    public GameObject ks1Object;
+    public GameObject ks2Object;
+    public GameObject ks3Object;
+    public GameObject ks4Object;
+    public GameObject ks5Object;
+    public GameObject ks6Object;
+    private KeySpawner[] spawners = new KeySpawner[6];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawners[0] = ks1Object.GetComponent<KeySpawner>();
+        spawners[1] = ks2Object.GetComponent<KeySpawner>();
+        spawners[2] = ks3Object.GetComponent<KeySpawner>();
+        spawners[3] = ks4Object.GetComponent<KeySpawner>();
+        spawners[4] = ks5Object.GetComponent<KeySpawner>();
+        spawners[5] = ks6Object.GetComponent<KeySpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
---
+++ Assets/Scripts/SpawningManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningManager : MonoBehaviour
+{
+    public GameObject ks1Object;
+    public GameObject ks2Object;
+    public GameObject ks3Object;
+    public GameObject ks4Object;
+    public GameObject ks5Object;
+    public GameObject ks6Object;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
