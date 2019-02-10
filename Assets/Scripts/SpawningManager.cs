2036255539a2498452b8ba3e6aba0a8b072e34fd
JDAV96 <arevalojose2015@outlook.com>
2019-02-09 16:25:21 -0800

    Fixing SpawningManager

--- Assets/Scripts/SpawningManager.cs
+++ Assets/Scripts/SpawningManager.cs
@@ -10,10 +10,17 @@ public class SpawningManager : MonoBehaviour
     public GameObject ks4Object;
     public GameObject ks5Object;
     public GameObject ks6Object;
+    private KeySpawner[] spawners = new KeySpawner[6];
 
     // Start is called before the first frame update
     void Start()
     {
+        spawners[0] = ks1Object.GetComponent<KeySpawner>();
+        spawners[1] = ks2Object.GetComponent<KeySpawner>();
+        spawners[2] = ks3Object.GetComponent<KeySpawner>();
+        spawners[3] = ks4Object.GetComponent<KeySpawner>();
+        spawners[4] = ks5Object.GetComponent<KeySpawner>();
+        spawners[5] = ks6Object.GetComponent<KeySpawner>();
     }
 
     // Update is called once per frame
