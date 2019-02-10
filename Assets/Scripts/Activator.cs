a85f9d279ac3cd30ea1d01990bcb97178c641c0a
JDAV96 <arevalojose2015@outlook.com>
2019-02-09 17:39:12 -0800

    Scoreboard

--- Assets/Scripts/Activator.cs
+++ Assets/Scripts/Activator.cs
@@ -7,12 +7,19 @@ public class Activator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AddScore();
+    }
+
+    //Replace call after your Destroy
+    //Also, right after you call AddScore(), write "active = flse" to deactivate AddScore() from bringing points just after a note is destroyed
+    void AddScore()
+    {
+        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + 100);
     }
 }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+
+    public string name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Text>().text = PlayerPrefs.GetInt(name) + "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
