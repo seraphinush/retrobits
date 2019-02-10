91c5da6959d596704161cd1637ecd633a811b1b6
seraphinush <seraphinus.hong@gmail.com>
2019-02-09 17:24:04 -0800

    reserve

--- Assets/Scripts/SpawningManager.cs
+++ Assets/Scripts/SpawningManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class SpawningManager : MonoBehaviour
 {
@@ -12,6 +13,12 @@ public class SpawningManager : MonoBehaviour
     public GameObject ks6Object;
     private KeySpawner[] spawners = new KeySpawner[6];
 
+    private int counter = 0;
+    private string songName = "..\\Songs\\lostWoods.json";
+    private int[][] song;
+
+    public string SongPath { get; private set; }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +28,34 @@ public class SpawningManager : MonoBehaviour
         spawners[3] = ks4Object.GetComponent<KeySpawner>();
         spawners[4] = ks5Object.GetComponent<KeySpawner>();
         spawners[5] = ks6Object.GetComponent<KeySpawner>();
+
+        LoadJson();
+    }
+
+    // Load song data as json
+    void LoadJson()
+    {
+        using (StreamReader sr = new StreamReader(songName))
+        {
+            string json = sr.ReadToEnd();
+            SongData data = JsonUtility.FromJson<SongData>(json);
+
+            song = data.songObject;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (counter < song.Length)
+        {
+            int[] temp = song[counter];
+            for (int i = 0; i < temp.Length; i++)
+            {
+                spawners[temp[i]].spawn();
+            }
+            counter++;
+        }
     }
 }
 
